882b7fc740aab8d2d33cd9f812953729c06116e8
uFafaa <rafaela_l_machado@estudante.sesisenai.org.br>
2026-06-11 11:31:41 -0300

    chave, segunda porta e portal

---
+++ Assets/Codigos/portal/PortalScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalScript : MonoBehaviour
+{
+    [Tooltip("Nome ou índice da cena para onde o jogador será teletransportado")]
+    public string nomeDaCenaDestino;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            
+            SceneManager.LoadScene(nomeDaCenaDestino);
+        }
+    }
+}
