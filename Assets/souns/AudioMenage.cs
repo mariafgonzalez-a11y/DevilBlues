3128f9a3d1bac3a18c1abb693eb9fd156939c549
Ryhanna Macedo <mylla.ryhanna@gmail.com>
2026-06-18 16:55:16 -0300

    som

---
+++ Assets/souns/AudioMenage.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class AudioMenage : MonoBehaviour
+{
+    public static AudioMenage instanceSound;
+
+     public AudioSource audioSource;
+
+     public AudioClip soundattack;
+    public AudioClip soundrun;
+    public AudioClip sounddeathPlayer;
+    public AudioClip soundenemy;
+    public AudioClip soundenviroment;
+    public AudioClip soundblow;
+
+
+
+    void Awake()
+    {
+        
+        if (instanceSound == null){
+
+            // Create a instance
+            instanceSound = this;
+
+            // Set to don't destroy
+            DontDestroyOnLoad(gameObject);
+
+        }else{
+
+            // Destroy the instance
+            Destroy(gameObject);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
