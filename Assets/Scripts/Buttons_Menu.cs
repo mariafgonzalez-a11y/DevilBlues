729521d9a4ba59dd18b8327c634d4e0d3da0c605
Marifer_VG <maria_f_gonzalez@estudante.sesisenai.org.br>
2026-06-29 02:04:34 -0300

    menu

--- Assets/Scripts/Buttons_Menu.cs
+++ Assets/Scripts/Buttons_Menu.cs
@@ -6,7 +6,7 @@ public class Buttons_Menu : MonoBehaviour
     void Jogar()
     {
 
-        SceneManager.LoadScene(nomeDoLevelDeJogo);
+        SceneManager.LoadScene(Fase1);
     }
     void SairJogo()
     {
